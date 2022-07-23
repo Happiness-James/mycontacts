ddf47d8bc059bc4772e1c6266ddd0de8f52fdb96
Happiness-James <gesarehjames@gmail.com>
2022-07-23 09:12:31 +0300

    commit

--- app/src/main/java/com/happinessjames/mycontacts/ContactRvAdapter.kt
+++ app/src/main/java/com/happinessjames/mycontacts/ContactRvAdapter.kt
@@ -1,10 +1,12 @@
 package com.happinessjames.mycontacts
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.happinessjames.mycontacts.databinding.ContactsListItemBinding
 import com.squareup.picasso.Picasso
@@ -26,6 +28,21 @@ class ContactRvAdapter (var contactsList: List<Contacts>): RecyclerView.Adapter<
             .centerCrop()
             .placeholder(R.drawable.ic_baseline_person_24)
             .into(holder.binding.ivContacts)
+        var context = holder.itemView.context
+        holder.binding.ivContacts.setOnClickListener{
+           Toast.makeText(context,"You have clicked the image",Toast.LENGTH_SHORT).show()
+        }
+        holder.binding.cardview.setOnClickListener{
+           val intent = Intent(context,ViewContactActivity::class.java)
+            intent.putExtra("ADDRESS", currentContacts.address)
+            intent.putExtra("NAME", currentContacts.name)
+            intent.putExtra("EMAIL", currentContacts.email)
+            intent.putExtra("PHONENUMBER", currentContacts.phoneNumber)
+            intent.putExtra("IMAGE", currentContacts.image)
+
+
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/happinessjames/mycontacts/ViewContactActivity.kt
+++ app/src/main/java/com/happinessjames/mycontacts/ViewContactActivity.kt
@@ -2,10 +2,42 @@ package com.happinessjames.mycontacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.happinessjames.mycontacts.databinding.ActivityViewContactBinding
+import com.squareup.picasso.Picasso
 
 class ViewContactActivity : AppCompatActivity() {
+    lateinit var binding: ActivityViewContactBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_contact)
+        binding = ActivityViewContactBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getExtras()
+    }
+    fun getExtras(){
+        var name = intent.extras?.getString("NAME")
+        var address = intent.extras?.getString("ADDRESS")
+        var email= intent.extras?.getString("EMAIL")
+        var phonenumber= intent.extras?.getString("PHONENUMBER")
+        var image = binding.ivProfile
+
+
+//        Toast.makeText(this,name,Toast.LENGTH_LONG).show()
+//        Toast.makeText(this,address,Toast.LENGTH_LONG).show()
+//        Toast.makeText(this,email,Toast.LENGTH_LONG).show()
+//        Toast.makeText(this,phonenumber,Toast.LENGTH_LONG).show()
+
+        binding.tvName1.text = name
+        binding.tvContact.text = email
+        binding.tvAddress1.text = address
+        binding.tvEmail.text = phonenumber
+
+        Picasso.get().load(intent.getStringExtra("IMAGE"))
+            .resize(1200,700)
+            .centerCrop()
+            .placeholder(R.drawable.ic_baseline_person_24)
+            .into(image)
+
     }
 }
